b34db8caa703d33aa24e7c25fd0e18dfa9d6c3d1
Stefan Riedel <stefan.rie@t-online.de>
2018-05-08 14:00:06 +0200

    First Version of a simple TCP connection to Websocket in Eclipse (in cooperation with Kay Bode) - change MainActive.java - change active_main.xml - update AndroidManifest.xml

--- app/src/main/java/research/dresden/htw/moderationapp/MainActivity.java
+++ app/src/main/java/research/dresden/htw/moderationapp/MainActivity.java
@@ -1,16 +1,64 @@
 package research.dresden.htw.moderationapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText e1;
+    private static Socket s;
+    private static PrintWriter printWriter;
+    String message = "";
+    private static String ip = "141.56.235.59"; //Achtung eigene IP-Adresse verwenden
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String name = "Stef";
+
+        e1 = (EditText)findViewById(R.id.editText);
+    }
+
+    public void send_text(View v) {
+        message = e1.getText().toString();
+
+        myTask mt = new myTask();
+        mt.execute();
+
+        Toast.makeText(getApplicationContext(), "Data send", Toast.LENGTH_LONG).show();
+
+    }
+
+
+    class myTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                s = new Socket(ip, 5000);       // connect to the socket at port 50000
+                printWriter = new PrintWriter(s.getOutputStream()); // set the output stream
+                printWriter.write(message);         // send the message through the socket
+                printWriter.flush();
+                printWriter.close();
+                s.close();
+
+
+
+
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 
     @Override
