336887a54d5343264ea4755c3f17de630a1cdd4e
mmw <maximilian.weidemann@contact-software.com>
2018-05-08 16:08:45 +0200

    Added MemberAdministration activities

---
+++ app/src/main/java/research/dresden/htw/moderationapp/MemberAdministratonActivity.java
@@ -0,0 +1,13 @@
+package research.dresden.htw.moderationapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberAdministratonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_administraton);
+    }
+}
