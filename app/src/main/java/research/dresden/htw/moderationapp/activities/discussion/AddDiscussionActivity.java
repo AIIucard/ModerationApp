e10e0562280fb1ab9a2250ad5ca5989684946315
mmw <maximilian.weidemann@contact-software.com>
2018-05-26 22:49:40 +0200

    Reworked Activities

---
+++ app/src/main/java/research/dresden/htw/moderationapp/activities/discussion/AddDiscussionActivity.java
@@ -0,0 +1,20 @@
+package research.dresden.htw.moderationapp.activities.discussion;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import research.dresden.htw.moderationapp.R;
+
+public class AddDiscussionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_discussion);
+    }
+
+}
